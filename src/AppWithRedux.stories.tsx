edeaa686593c3b3f2054b070b598dedab34164a9
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-01-26 22:44:10 +0300

    lesson 12 add storybook

---
+++ src/AppWithRedux.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import AppWithRedux from './AppWithRedux';
+import {ReduxStoreProviderDecorator} from './stories/ReduxStoreProviderDecorator';
+
+export default {
+    title: 'TodoLists/AppWithRedux',
+    component: AppWithRedux,
+    decorators: [ReduxStoreProviderDecorator]
+} as ComponentMeta<typeof AppWithRedux>;
+
+
+const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux/>;
+
+export const AppWithReduxStory = Template.bind({});
+
+
--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useCallback, useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {TextField} from '@material-ui/core';
 
 type EditableSpanPropsType = {
@@ -24,6 +24,6 @@ export const EditableSpan = React.memo((props: EditableSpanPropsType) => {
     }
 
     return editMode
-        ?    <TextField value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode} />
+        ? <TextField value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}/>
         : <span onDoubleClick={activateEditMode}>{props.value}</span>
 })
