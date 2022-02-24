a0dc1200b40a7aee8cf6c9722e1cfe4b05457bca
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-02-24 22:46:49 +0300

    lesson 16. task completed

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -36,14 +36,41 @@ export const todolistsAPI = {
     }
 }
 
+export const authAPI = {
+    login( data: LoginParamsType) {
+        return instance.post<LoginParamsType, AxiosResponse<ResponseType<{ userId: number }>>>('/auth/login', data)
+    },
+    me() {
+        return instance.get<ResponseType<MeParamsType>>('/auth/me')
+    },
+    logout() {
+        return instance.delete<ResponseType>('/auth/login')
+    },
+}
+
 // types
+export type MeParamsType = {
+    id: number
+    email:string
+    login: string
+}
+
+export type LoginParamsType = {
+    email: string
+    password: string
+    rememberMe?: boolean
+    captcha?: string
+}
+
+
+
 export type TodolistType = {
     id: string
     title: string
     addedDate: string
     order: number
 }
-export type ResponseType< D = {}> = {
+export type ResponseType<D = {}> = {
     resultCode: number
     messages: Array<string>
     fieldsErrors: Array<string>
