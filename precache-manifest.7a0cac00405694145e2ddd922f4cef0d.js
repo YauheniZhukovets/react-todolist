8c538dfa236f3246a67d945b8f7611fdb0cad8ff
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-06-06 00:02:13 +0300

    Updates

---
+++ precache-manifest.7a0cac00405694145e2ddd922f4cef0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad00f5948f1ff49f95f434c5d0fd6c7",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "5dcd5801d38524c0ed37",
+    "url": "/react-todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "7a5c5714e3fb3897635b",
+    "url": "/react-todolist/static/js/2.8396a27c.chunk.js"
+  },
+  {
+    "revision": "edd9a6d0fd78f0d308519aaec3144040",
+    "url": "/react-todolist/static/js/2.8396a27c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dcd5801d38524c0ed37",
+    "url": "/react-todolist/static/js/main.83bd7bed.chunk.js"
+  },
+  {
+    "revision": "c8e9edcc0b7d98c328a3",
+    "url": "/react-todolist/static/js/runtime-main.5ff039be.js"
+  }
+]);
